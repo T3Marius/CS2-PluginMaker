a57356937e32c4b803f40fb8fab4925111a7c2ee
T3Marius <168598779+T3Marius@users.noreply.github.com>
2024-10-27 20:35:43 +0200

    Update PluginMaker.cs

--- PluginMaker.cs
+++ PluginMaker.cs
@@ -6,7 +6,7 @@ namespace PluginMaker
     {
         public override string ModuleAuthor => "T3Marius";
         public override string ModuleName => "[ Plugin Maker ]  ";
-        public override string ModuleVersion => "1.1";
+        public override string ModuleVersion => "1.0";
         public override string ModuleDescription => "Easily create menus, submenus, welcome messages, announcements, and commands from separate configs.";
         public static PluginMaker Instance { get; set; } = new();
         public override void Load(bool hotReload)
